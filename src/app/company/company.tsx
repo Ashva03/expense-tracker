000c4d86e364dd9b31e76cdfac8e98655e1616f2
vsahil2002 <ssvasoya901@gmail.com>
2024-08-06 07:06:45 +0530

    add antd tabel

--- src/app/company/company.tsx
+++ src/app/company/company.tsx
@@ -1,68 +1,111 @@
-"use client";
-import React, { useCallback, useState } from 'react'
-import { CompanyHeader, Content, HeaderName, ItemName, ListItem, ListMain, MainDiv, NameTitle, NewButtonMain, TableHeader } from './style'
+'use client';
+import React, { useCallback, useState } from 'react';
+import { CompanyHeader, Content, HeaderName, MainDiv, NewButtonMain } from './style';
 import Button from '../components/button/button';
 import ModalCustom from '../components/modelCustom/modelCustom';
 import AddNewCompanyModal from './addNewCompanyModal';
+import { Space, Table, TableProps, Tag } from 'antd';
+import { EllipsisOutlined } from '@ant-design/icons';
 
-function Company() {
-    const [isNewCompanyModal, setIsNewCompanyModal] = useState(false);
-
-    const onClickNew = useCallback(() => {
-        setIsNewCompanyModal(true)
-    }, [])
-
-    const onCloseNewModal = useCallback(() => {
-        setIsNewCompanyModal(false)
-    }, [])
-
-    return (
-        <>
-            <MainDiv>
-                <Content>
-                    <CompanyHeader>
-                        <HeaderName>
-                            Company
-                        </HeaderName>
-                        <NewButtonMain>
-                            <Button onClick={onClickNew} title='New' />
-                        </NewButtonMain>
-                    </CompanyHeader>
-                    <ListMain>
-                        <TableHeader>
-                            {/* <NameHeader></NameHeader> */}
-                            <NameTitle>Name</NameTitle>
-                        </TableHeader>
-                        <ListItem>
-                            <ItemName>Ambani</ItemName>
-                            <div>three dots</div>
-                        </ListItem>
-                        <ListItem>
-                            <ItemName>Adani</ItemName>
-                            <div>three dots</div>
-                        </ListItem>
-                        <ListItem>
-                            <ItemName>Jio</ItemName>
-                            <div>three dots</div>
-                        </ListItem>
-                        <ListItem>
-                            <ItemName>Ola</ItemName>
-                            <div>three dots</div>
-                        </ListItem>
-                        <ListItem>
-                            <ItemName>Home</ItemName>
-                            <div>three dots</div>
-                        </ListItem>
-                        <ListItem>
-                            <ItemName>Personal</ItemName>
-                            <div>three dots</div>
-                        </ListItem>
-                    </ListMain>
-                </Content>
-            </MainDiv>
-            <ModalCustom open={isNewCompanyModal} onClose={onCloseNewModal}><AddNewCompanyModal onClose={onCloseNewModal} /></ModalCustom>
-        </>
-    )
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+  tags: string[];
 }
 
-export default Company
+const columns: TableProps<DataType>['columns'] = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: () => (
+      <Space size='middle'>
+        <EllipsisOutlined style={{ fontSize: '24px', cursor: 'pointer' }} />
+      </Space>
+    ),
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+function Company() {
+  const [isNewCompanyModal, setIsNewCompanyModal] = useState(false);
+
+  const onClickNew = useCallback(() => {
+    setIsNewCompanyModal(true);
+  }, []);
+
+  const onCloseNewModal = useCallback(() => {
+    setIsNewCompanyModal(false);
+  }, []);
+
+  return (
+    <>
+      <MainDiv>
+        <Content>
+          <CompanyHeader>
+            <HeaderName>Company</HeaderName>
+            <NewButtonMain>
+              <Button onClick={onClickNew} title='New' />
+            </NewButtonMain>
+          </CompanyHeader>
+          <Table columns={columns} dataSource={data} />
+        </Content>
+      </MainDiv>
+      <ModalCustom open={isNewCompanyModal} onClose={onCloseNewModal}>
+        <AddNewCompanyModal onClose={onCloseNewModal} />
+      </ModalCustom>
+    </>
+  );
+}
+
+export default Company;
--- src/app/company/style.tsx
+++ src/app/company/style.tsx
@@ -1,84 +1,59 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const MainDiv = styled.div`
-    display: flex;
-    height: 100vh - 88px !important;
-    margin-top: 88px;
-`
+  display: flex;
+  height: calc(100vh - 88px);
+  margin-top: 88px;
+`;
 
 const Content = styled.div`
-    width: 100%;
-    overflow: auto;
-    padding: 0 24px;
-`
+  width: 100%;
+  overflow: auto;
+  padding: 0 124px;
+
+  @media (max-width: 768px) {
+    padding: 0 40px;
+  }
+
+  @media (max-width: 576px) {
+    padding: 0 20px;
+  }
+`;
 
 const CompanyHeader = styled.div`
-    width: 100%;
-    overflow: auto;
-    display: flex;
-    margin-top: 20px;
-    justify-content: space-between;
-    @media (max-width: 449px) {
-    margin: 0;
-    width: auto;
-    }
-`
+  width: 100%;
+  display: flex;
+  margin-top: 20px;
+  justify-content: space-between;
+
+  @media (max-width: 576px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`;
 
 const NewButtonMain = styled.div`
-    display: flex;
-    justify-content: flex-end;
-`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 16px;
+
+  @media (max-width: 576px) {
+    width: 100%;
+    justify-content: flex-start;
+    margin-top: 10px;
+  }
+`;
 
 const HeaderName = styled.div`
-    font-size: 20px;
-    font-weight: 600;
-    align-content: center;
-    margin-left: 30px;
-    @media (max-width: 449px) {
+  font-size: 20px;
+  font-weight: 600;
+  align-content: center;
+
+  @media (max-width: 576px) {
     margin: 0;
     width: auto;
-    }
-`
+    font-size: 18px;
+  }
+`;
 
-const ListMain = styled.div`
-    background-color: #ededd2;
-    margin: 15px;
-    :first-child{
-        border-top-right-radius: 10px;
-        border-top-left-radius: 10px;
-    }
-    border: 1px solid gray;
-    border-radius: 10px;
-    :last-child{
-        border: 0px;
-    }
-`
-
-const ListItem = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    padding: 8px;
-    gap:30px;
-    border-bottom: 1px solid gray;
-`
-
-const ItemName = styled.div`
-    margin-left: 5px;
-`
-
-const TableHeader = styled.div`
-    background-color: gray;
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    padding: 8px;
-`
-
-const NameTitle = styled.div`
-    padding-left: 8px;
-    font-weight: 400;
-    color: white;
-`
-
-export { MainDiv, Content, CompanyHeader, NewButtonMain, HeaderName, ListMain, ListItem, ItemName, TableHeader, NameTitle }
+export { MainDiv, Content, CompanyHeader, NewButtonMain, HeaderName };
